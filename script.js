de3111acc02bd761f09b99088d2add9b16cde022
M. Rizqi Bahar <16.9249@stis.ac.id>
2021-04-08 22:56:31 +0700

    wide

---
+++ script.js
@@ -0,0 +1,6 @@
+var email = document.getElementsByTagName('email').value;
+var pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+function check() {
+
+
+}
