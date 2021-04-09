506991f138b48d6eff217a1a0bac5db930e44aae
M. Rizqi Bahar <16.9249@stis.ac.id>
2021-04-09 10:17:29 +0700

    desktop fix

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-var email = document.getElementsByTagName('email').value;
-var pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-function check() {
+window.onload = function () {
+
+    var email = document.getElementById('emailInput');
+    var pattern = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+    let button = document.getElementById('validate')
+    let msg = document.getElementById('msg')
+
+    button.addEventListener('click', function (event) {
+        if (pattern.test(email.value)) {
+            msg.innerHTML =
+                '<p class="msg-true">thanks for subscribe</p>'
+
+
+
+        } else {
+            msg.innerHTML =
+                '<p class="msg-false">check your email please</p>'
+
+
+        }
+        event.preventDefault()
+    })
 
 
 }
